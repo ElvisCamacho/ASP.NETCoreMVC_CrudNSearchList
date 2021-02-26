6f479140997dd2a7260d4025ef06555f382a40a2
camachocv@hotmail.com <elvi0506@edu.easj.dk>
2021-02-26 13:26:39 +0100

    small things

---
+++ Components/StudentCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudOperation.Interfaces;
+using CrudOperation.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CrudOperationASP.NETMVCCore5.Components
+{
+    public class StudentCount: ViewComponent
+    {
+        // creata a class that much a business logic
+        //2: inherit base class viwComponent
+
+        private IStudentRepository _StudentRepo;
+
+        public StudentCount(IStudentRepository studentRepo)
+        {
+            _StudentRepo = studentRepo;
+        }
+        // view invoke logic
+        public IViewComponentResult Invoke()
+        {
+            var sCount = _StudentRepo.GetAllStudents().Count;
+            var vm = new StudentVm
+            {
+                TotalCount = sCount
+            };
+
+            //3: create a view component razor page
+            // 4:pass data into component
+            return View(sCount);
+        }
+
+
+
+    }
+}
